71d2aade151d8bb6278a62daa7a2b1f6757f531f
5tr1ker <tjseocld@naver.com>
2022-12-29 00:57:40 +0900

    feat: ULID 일련번호 코드 구현
    ULID 일련변호 코드 구현

--- resource.cpp
+++ resource.cpp
@@ -2,8 +2,41 @@
 
 void resourceManagement::createResource() {
 	// ULID , 자산 이미지 , 자산명 , 분류 , 최초 등록 일시 , 상태 , 위치 , 사용직원 , 최종 수정 일시 , 구매처 , 최초 구매일 , 가격
-	srand((unsigned int)time(NULL));
-	int ulid = rand() * 1000000000;
+	MYSQL* conn, connection;
+	MYSQL_RES* result;
+	MYSQL_ROW row;
 
+	char DB_HOST[] = "localhost";
+	char DB_USER[] = "root";
+	char DB_PASS[] = "password";
+	char DB_NAME[] = "comon";
+
+	// DB 커넥션 연결
+	mysql_init(&connection);
+	conn = mysql_real_connect(&connection, DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306, (char*)NULL, 0);
+	char sql[1024];
+
+
+	string ulid;
+	while (true) {
+		srand((unsigned int)time(NULL));
+		ulid = "";
+		for (int i = 0; i < 10; i++) { // 일련번호 생성
+			std::string alphabet = "570968123405981647230908243765175896420312983645071";
+			char ch = alphabet[rand() % alphabet.size()];
+			ulid += ch;
+		}
+		string query = "select ulid from system_infos where ulid = '" + ulid + "';";
+		strcpy_s(sql, query.c_str());
+
+		if (mysql_query(conn, sql) == 0) { // 일련변호가 겹치는지 확인
+			result = mysql_store_result(conn);
+			if ((row = mysql_fetch_row(result)) == NULL) { // 겹치지 않을 경우 종료
+				break;
+			};
+			mysql_free_result(result);
+		}
+	}
+	
 	cout << ulid;
 }
